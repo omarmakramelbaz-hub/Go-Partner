5ed2653e982d4adc3c14938b6cb4ca68474c32a3
omarmakramelbaz-hub <omarmakramelbaz@gmail.com>
2026-09-20 06:16:55 +0300

    Add approved partner activation endpoint

--- lib/helpers/networking/urls.dart
+++ lib/helpers/networking/urls.dart
@@ -107,6 +107,7 @@ class Urls {
   static const String vendorSignUp = '${baseUrl}signup';
   static const String partnerApplications = '${baseUrl}partner-applications';
   static const String partnerApplicationStatus = '${baseUrl}partner-applications/status';
+  static const String partnerActivate = '${baseUrl}partner-applications/activate';
   static const String delegateServiceRequests = '${baseUrl}delegate/service-requests';
   static String updateDelegateServiceRequest(int id) => '${baseUrl}delegate/service-requests/$id/status';
   static const String professions = '${baseUrl}professions';
