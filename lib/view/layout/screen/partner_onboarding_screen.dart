735fcd53643c84eb337ec607191c2b6f4c6fee51
omarmakramelbaz-hub <omarmakramelbaz@gmail.com>
2026-09-21 03:34:28 +0300

    Fix GO Partner onboarding LoginScreen import

--- lib/view/layout/screen/partner_onboarding_screen.dart
+++ lib/view/layout/screen/partner_onboarding_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../../../helpers/utils/navigator_methods.dart';
-import '../../auth/screen/login_screen.dart';
+import '../auth/screen/login_screen.dart';
 
 class PartnerOnboardingScreen extends StatelessWidget {
   const PartnerOnboardingScreen({super.key});
