1fe63a4abb0e4f3793901d187a0629416551cd64
omarmakramelbaz-hub <omarmakramelbaz@gmail.com>
2026-09-25 04:42:44 +0300

    Add Go Drive price offer endpoint

--- lib/helpers/networking/urls.dart
+++ lib/helpers/networking/urls.dart
@@ -78,6 +78,7 @@ class Urls {
   static const String delegateOrderDetails = '${baseUrl}delegate/orders/';
   static const String delegateHomeCurrentOrder = '${baseUrl}delegate/orders?home=yes&status=current';
   static const String delegateAcceptDecline = '${baseUrl}delegate/accept_decline/orders/';
+  static String delegateShippingOffer(int orderId) => '${baseUrl}delegate/shipping/orders/$orderId/offer';
   static const String compleatOrderDelegate = '${baseUrl}delegate/orders/';
   static const String delegateCurrentOrders = '${baseUrl}delegate/orders?status=current';
   static const String delegateReports = '${baseUrl}delegate/reports';
